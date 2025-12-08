db6138f599004ac10fa3067f7a16c571169a6a1e
Vixel2006 <yusufshihata2006@gmail.com>
2025-12-08 19:29:41 +0200

    completing the bag of concepts implemenation

--- src/models/bag_of_concepts.py
+++ src/models/bag_of_concepts.py
@@ -11,16 +11,21 @@ class BagOfConcepts(nn.Module):
         self.concepts.weight.data.uniform_(-1/self.num_concepts, 1/self.num_concepts)
 
     def forward(self, inp: torch.Tensor) -> torch.Tensor:
-        # NOTE: Here we should be aware that this concept bag can take any form of modality as input
-        flat_concept = inp.reshape(-1, self.config.concept_dim)
+        flat_inp = inp.reshape(-1, self.config.concept_dim) # (batch_size * num_slots, concept_dim)
 
-        distances = torch.cdist(flat_concept, self.concepts.weight)
+        distances = torch.cdist(flat_inp, self.concepts.weight)
 
         encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
 
-        quantized_flat = torch.index_select(self.concepts.weight, 0, encoding_indices.view(-1))
+        concepts_flat = torch.index_select(self.concepts.weight, 0, encoding_indices.view(-1))
 
-        quantized = quantized_flat.view_as(inputs)
+        concepts = concepts_flat.view_as(inputs)
+
+        # NOTE: Here we do this trick so we can pass the discrete concepts but,
+        # make the inputs passed through the backward prop to avoid the non-differntiable nature of the argmin
+        # here the detach makes it explicit that (concepts - inputs) is freezed in terms of the backward prop
+        # This is equavelent to saying that dL/d(concepts) = dL/d(inputs)
+        concepts = inputs + (concepts - inputs).detach()
 
         return concept
 
