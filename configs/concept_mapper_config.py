c3103c2d0652abaa124303be38ef767cfd9f3f3c
Vixel2006 <yusufshihata2006@gmail.com>
2025-12-12 09:55:54 +0200

    small fix

--- configs/concept_mapper_config.py
+++ configs/concept_mapper_config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from .image_encoder_config import ImageEncoderConfig
 from .text_encoder_config import TextEncoderConfig
 from .bag_of_concepts_config import BagOfConceptsConfig
@@ -6,8 +6,8 @@ from .bag_of_concepts_config import BagOfConceptsConfig
 @dataclass
 class ConceptMapperConfig:
     modality: str
-    image_encoder_config: ImageEncoderConfig = ImageEncoderConfig(default_factory=ImageEncoderConfig)
-    text_encoder_config: TextEncoderConfig = TextEncoderConfig(default_factory=TextEncoderConfig)
-    bag_of_concepts_config: BagOfConceptsConfig = BagOfConceptsConfig(default_factory=BagOfConceptsConfig)
+    image_encoder_config: ImageEncoderConfig = field(default_factory=ImageEncoderConfig)
+    text_encoder_config: TextEncoderConfig = field(default_factory=TextEncoderConfig)
+    bag_of_concepts_config: BagOfConceptsConfig = field(default_factory=BagOfConceptsConfig)
     num_attention_heads: int = 4
     multi_head_attention_dropout_rate: float = 0.3
